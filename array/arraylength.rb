23cc228eba0e388332db4e41e7ef4f98d098624a
rajukaji <rajulamarainon@gmail.com>
2025-07-28 17:59:00 +0545

    length and reverse of array in ruby

---
+++ array/arraylength.rb
@@ -0,0 +1,4 @@
+arraylist = [1, 2, 3, 4, 5]
+
+puts arraylist.length  # Outputs the length of the array, which is 5
+puts arraylist.size    # Also outputs the length of the array, which is 5
---
+++ array/reversearray.rb
@@ -0,0 +1,7 @@
+arrayList = [1, 2, 3, 4, 5]
+
+print arrayList.reverse  # Outputs the reversed array: [5, 4, 3, 2, 1]
+
+print "\n"
+
+print arrayList
