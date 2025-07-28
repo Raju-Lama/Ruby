f6b1d2650ea7467e8fb0c35c93c17111865f4944
rajukaji <rajulamarainon@gmail.com>
2025-07-28 17:02:59 +0545

    array

---
+++ array/basic.rb
@@ -0,0 +1,7 @@
+arrayList = [1, 2, 3, 4, 5]
+
+arrayList2 = Array.new(5, 0) # creates an array of size 5 with all elements initialized to 0
+# 5 is size, 0 is default value
+
+puts arrayList
+puts arrayList2
