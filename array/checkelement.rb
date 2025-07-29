c95a872223cc1002e861c10e5a70464d9878fb4b
rajukaji <rajulamarainon@gmail.com>
2025-07-29 20:29:38 +0545

    ruby works

---
+++ array/checkelement.rb
@@ -0,0 +1,5 @@
+# check element in array
+
+array = [1, 2, 3, 4, 5]
+
+print "if 5 is in the array : ", array.include?(5)
---
+++ array/listmethods.rb
@@ -0,0 +1,16 @@
+arrayList = Array.new()
+
+arrayList.push(5)
+# appeands 5 to the end of the array
+
+arrayList.push(6, 2, 3, 1)
+
+print arrayList
+
+arrayList.pop() # Removes the last element
+
+arrayList.shift() # Removes the first element
+
+arrayList.unshift(0) # Adds 0 at the beginning of the array
+
+print "\n", arrayList
